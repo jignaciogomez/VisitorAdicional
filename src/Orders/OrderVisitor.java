d0f0c82dae060111c14195f0859c5b9251206124
lcmtheorigen <lcmtheorigen@hotmail.com>
2018-05-09 20:06:35 -0500

    Versión Final

--- src/Orders/OrderVisitor.java
+++ src/Orders/OrderVisitor.java
@@ -4,31 +4,31 @@ import java.util.*;
 
 class OrderVisitor implements VisitorInterface{
   private Enumeration orderObjList;
-  private Vector ordeObjVector;
+  private Vector orderObjVector;
   
   public Enumeration getAllOrders(){
-      return ordeObjVector.elements();
+      return orderObjVector.elements();
   }
 
   public OrderVisitor() {
-    ordeObjVector = new Vector();
+    orderObjVector = new Vector();
   }
  
   public void visit(NonCaliforniaOrder inp_order) {
     inp_order.setTotal(inp_order.getOrderAmount());
-     ordeObjVector.add(inp_order);
+     orderObjVector.add(inp_order);
   }
   public void visit(CaliforniaOrder inp_order) {
       inp_order.setTotal(inp_order.getOrderAmount()+inp_order.getAdditionalTax());
-      ordeObjVector.add(inp_order);
+      orderObjVector.add(inp_order);
   }
   public void visit(ColombianOrder inp_order) {
       inp_order.setTotal(inp_order.getOrderAmount()+inp_order.getIvaTax()); 
-      ordeObjVector.add(inp_order);
+      orderObjVector.add(inp_order);
   }
   public void visit(OverseasOrder inp_order) {
       inp_order.setTotal(inp_order.getOrderAmount()+inp_order.getAdditionalSH());
-      ordeObjVector.add(inp_order);
+      orderObjVector.add(inp_order);
   }
   
   public double getOrderTotal() {
